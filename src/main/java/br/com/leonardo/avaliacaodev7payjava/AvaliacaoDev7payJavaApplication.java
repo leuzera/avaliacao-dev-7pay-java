721664f2627f769e97a0280b0fd9ffe6baf7537e
Leonardo Fleury <fleuryleomoraes@gmail.com>
2024-04-26 18:09:35 -0300

    Primeiro commit

---
+++ src/main/java/br/com/leonardo/avaliacaodev7payjava/AvaliacaoDev7payJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.leonardo.avaliacaodev7payjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaoDev7payJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvaliacaoDev7payJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/leonardo/avaliacaodev7payjava/AvaliacaoDev7payJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leonardo.avaliacaodev7payjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaoDev7payJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
