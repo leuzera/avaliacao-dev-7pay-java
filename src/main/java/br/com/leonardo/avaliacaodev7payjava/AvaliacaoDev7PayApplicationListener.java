e9945274b1a60ed408f0ccb5bc29599a63285c6b
Leonardo Fleury <fleuryleomoraes@gmail.com>
2024-04-29 23:32:48 -0300

    Em ambiente de teste, despesas não são baixadas do site

--- src/main/java/br/com/leonardo/avaliacaodev7payjava/AvaliacaoDev7PayApplicationListener.java
+++ src/main/java/br/com/leonardo/avaliacaodev7payjava/AvaliacaoDev7PayApplicationListener.java
@@ -1,24 +1,38 @@
 package br.com.leonardo.avaliacaodev7payjava;
 
-import br.com.leonardo.avaliacaodev7payjava.infrastructure.recife.RecifeApi;
+import br.com.leonardo.avaliacaodev7payjava.infrastructure.recife.RecifeService;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.function.Predicate;
 
 @Component
 @Order(0)
 public class AvaliacaoDev7PayApplicationListener implements ApplicationListener<ApplicationReadyEvent> {
-    private final RecifeApi recifeApi;
+    private final RecifeService recifeService;
 
-    public AvaliacaoDev7PayApplicationListener(RecifeApi recifeApi) {
-        this.recifeApi = recifeApi;
+    public AvaliacaoDev7PayApplicationListener(RecifeService recifeService) {
+        this.recifeService = recifeService;
     }
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
+        Environment env = event.getApplicationContext().getEnvironment();
+
+        if (isTest(env)) {
+            return;
+        }
+
         System.out.println("Carregando lista de Despesas Orçamentárias de Recife...");
-        Integer saved = recifeApi.listaESalvaDespesaOrcamentaria(100);
+        Integer saved = recifeService.listaESalvaDespesaOrcamentaria(100);
         System.out.println("Lista carregada. " + saved.toString() + " despesas salvas.");
+    }
+
+    private static boolean isTest(Environment env) {
+        return Arrays.stream(env.getActiveProfiles()).anyMatch(Predicate.isEqual("test"));
     }
 }
